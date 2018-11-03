548fb493bb4355d55ed9db6909e65efc06928006
Leonardo Gomes <lgdevlop@gmail.com>
2018-11-03 01:28:16 -0200

    Criado o schema das migrations para criação das tabelas do banco de dados e inserido o nome do host do banco de dados.

--- versao-inicial/backend/config/db.js
+++ versao-inicial/backend/config/db.js
@@ -1,4 +1,6 @@
 const config = require("../knexfile");
 const knex = require("knex")(config);
 
+// Executa as migrations assim que a API é carregada - Não recomendado
+knex.migrate.latest([config])
 module.exports = knex;
--- versao-inicial/backend/knexfile.js
+++ versao-inicial/backend/knexfile.js
@@ -1,6 +1,8 @@
 module.exports = {
   client: "postgresql",
   connection: {
+    host : 'dbpostgres',
+    port: 5432,
     database: "knowledge",
     user: "postgres",
     password: "123456"
