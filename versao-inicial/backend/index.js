337a179ebceeb992a59ce6191cb63c71e71653d5
Leonardo Gomes <lgdevlop@gmail.com>
2018-11-04 17:57:45 -0200

    Criado na API as opções de salvar e consultar um usuário, bem como as rotas de consulta, inserção e atualização do mesmo.

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,15 +1,16 @@
-const app = require("express")();
-const consign = require("consign");
-const db = require("./config/db");
+const app = require('express')();
+const consign = require('consign');
+const db = require('./config/db');
 
 app.db = db;
 
 consign()
-  .then("./config/middlewares.js")
-  .then("./api")
-  .then("./config/routes.js")
+  .then('./config/middlewares.js')
+  .then('./api/validation.js')
+  .then('./api')
+  .then('./config/routes.js')
   .into(app);
 
 app.listen(4000, () => {
-  console.log("aplicativo escutando na porta 4000");
+  console.log('aplicativo escutando na porta 4000');
 });
